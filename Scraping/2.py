9881c5d2d9678775ed9d977c8a4528b6cebb0de0
Manav <manavratnani08@gmail.com>
2024-07-01 12:42:25 +0530

    Initial commmit

--- Scraping/2.py
+++ Scraping/2.py
@@ -1,21 +1,5 @@
 import pandas as pd
 
-# election_results_path = r"C:\Users\Khsak\Downloads\scrapped data\election_results.xlsx"
-# leading_results_path = r"C:\Users\Khsak\Downloads\scrapped data\leading_results.xlsx"
-
-# election_df = pd.read_excel(election_results_path)
-# leading_df = pd.read_excel(leading_results_path)
-
-# if len(election_df) != len(leading_df):
-#     print("Warning: The number of rows in the two DataFrames does not match.")
-
-
-# merged_df = pd.concat([election_df, leading_df], axis=1)
-# merged_file_path = r"C:\Users\Khsak\Downloads\scrapped data\merged_election_results.xlsx"
-
-# merged_df.to_excel(merged_file_path, index=False)
-# print(f"Merged Excel file has been created at: {merged_file_path}")
-
 election_results_file =r'C:\Users\hp\OneDrive\Desktop\kvtask\election_results.xlsx'
 nda_parties_file = r'C:\Users\hp\OneDrive\Desktop\kvtask\NDA_parties.xlsx'
 independent_parties_file = r'C:\Users\hp\OneDrive\Desktop\kvtask\Independent_parties.xlsx'
